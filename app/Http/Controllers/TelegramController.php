d6638d385ca1d38c24fc75cd83f84e6d6265e953
Adil Abdu Bushra <adilabdu68@gmail.com>
2021-12-02 13:07:14 +0900

    Add new Users to database, existing Users are identified

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -3,14 +3,24 @@
 namespace App\Http\Controllers;
 
 use BotMan\BotMan\BotMan;
-use Illuminate\Http\Request;
+use App\User;
 
 class TelegramController extends Controller
 {
     
     public function start(BotMan $bot) {
-        $user = $bot->getUser();
-        $bot->reply('Congrats '.$user->getId().', you have triggered the command successfully!');
+        
+        $userID = $bot->getUser()->getId();
+        
+        if(User::where('telegram_id', $userID)->first()) {
+            $bot->reply('You have already started the bot!');
+        } else {
+            User::create([
+                "telegram_id" => $userID
+            ]);
+            $bot->reply('Congrats '.$userID.', you have triggered the command successfully!');
+
+        }
     }
 
     public function fallback(BotMan $bot) {
