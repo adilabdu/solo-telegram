88c0741516d5e7c5f58c23c8125f405d786a199f
Adil Abdu Bushra <adilabdu68@gmail.com>
2021-12-02 13:23:26 +0900

    Testing eloquent Model create

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -11,16 +11,23 @@ class TelegramController extends Controller
     public function start(BotMan $bot) {
         
         $userID = $bot->getUser()->getId();
-        
-        if(User::where('telegram_id', $userID)->exists()) {
-            $bot->reply('You have already started the bot!');
-        } else {
-            User::create([
-                "telegram_id" => $userID
-            ]);
-            $bot->reply('Congrats '.$userID.', you have triggered the command successfully!');
 
-        }
+        User::create([
+            "telegram_id" => $userID,
+            "name" => $bot->getUser()->getFirstName()
+        ]);
+
+        $bot->reply('User created, with name ' . $bot->getUser()->getFirstName());
+
+        // if(User::where('telegram_id', $userID)->exists()) {
+        //     $bot->reply('You have already started the bot!');
+        // } else {
+        //     User::create([
+        //         "telegram_id" => $userID
+        //     ]);
+        //     $bot->reply('Congrats '.$userID.', you have triggered the command successfully!');
+
+        // }
     }
 
     public function fallback(BotMan $bot) {
