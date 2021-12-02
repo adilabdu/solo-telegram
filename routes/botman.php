216fa2ae7db355acfdb6c31da9fd0c232201bcea
Adil Abdu Bushra <adilabdu68@gmail.com>
2021-12-02 16:04:48 +0900

    Updated help command

--- routes/botman.php
+++ routes/botman.php
@@ -4,11 +4,13 @@ use App\Http\Controllers\TelegramController;
 
 $botman = resolve('botman');
 
-$botman->hears('Hi', function ($bot) {
-    $bot->reply('Hello!');
-});
-$botman->hears('Start conversation', BotManController::class.'@startConversation');
+// $botman->hears('Hi', function ($bot) {
+//     $bot->reply('Hello!');
+// });
+// $botman->hears('Start conversation', BotManController::class.'@startConversation');
 
 $botman->hears('/start', TelegramController::class.'@start');
+
+$botman->hears('/help', TelegramController::class.'@help');
 
 $botman->fallback(TelegramController::class.'@fallback');
